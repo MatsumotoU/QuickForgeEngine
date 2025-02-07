f0418ffa528e7b9547823271380629554df692f4
MatsumotoU <k024g0066@g.neec.ac.jp>
2025-02-07 15:44:57 +0900

    ウィンドウ生成をクラス化

---
+++ Engine/Class/Base/Windows/WinApp.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <Windows.h>
+#include <cstdint>
+
+class WinApp
+{
+public:
+	// ウィンドウサイズ
+	static const int kWindowWidth = 1280; // 横幅
+	static const int kWindowHeight = 720; // 縦幅
+	// ウィンドウクラス名
+	static const wchar_t kWindowClassName[];
+
+public:
+	static WinApp* GetInstance();
+
+	/// <summary>
+	/// ウィンドウプロシージャ
+	/// </summary>
+	/// <param name="hwnd">ウィンドウハンドル</param>
+	/// <param name="msg">メッセージ番号</param>
+	/// <param name="wparam">メッセージ情報1</param>
+	/// <param name="lparam">メッセージ情報2</param>
+	/// <returns>成否</returns>
+	static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+public:
+	/// <summary>
+	/// ゲームウィンドウの作成
+	/// <param name="title">ウィンドウタイトル</param>
+	/// <param name="windowStyle">ウィンドウの初期スタイル</param>
+	/// <param name="clientWidth">ウィンドウのクライアント領域の初期幅</param>
+	/// <param name="clientHeight">ウィンドウのクライアント領域の初期高さ</param>
+	/// </summary>
+	void CreateGameWindow(int32_t clientWidth = kWindowWidth, int32_t clientHeight = kWindowHeight);
+
+private:
+	WinApp() = default;
+	~WinApp() = default;
+	WinApp(const WinApp&) = delete;
+	WinApp& operator=(const WinApp&) = delete;
+
+private:
+	WNDCLASS wc{};
+	
+};
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#include <Windows.h>
-#include <cstdint>
-#include "Class/Base/MyString.h"
+#include "Engine/Class/Base/Windows/WinApp.h"
+#include "Engine/Class/Base/MyString.h"
 
 #include <d3d12.h>
 #include <dxgi1_6.h>
@@ -9,65 +8,12 @@
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 
-// ウィンドウプロージャー
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
-	WPARAM wparam, LPARAM lparam) {
-	// メッセージに応じてゲーム固有の処理を行う
-	switch (msg) {
-		// ウィンドウが破棄された
-	case WM_DESTROY:
-		// OSに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	// 標準のメッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
 // windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	WNDCLASS wc{};
-	// ウィンドウプロージャー
-	wc.lpfnWndProc = WindowProc;
-	// ウィンドウクラス名
-	wc.lpszClassName = L"CG2WindowClass";
-	// インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-	// カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-
-	// ウィンドウクラスを登録する
-	RegisterClass(&wc);
-
-	// クライアント領域のサイズ
-	const int32_t kClientWidth = 1280;
-	const int32_t kClientHeight = 720;
-
-	// ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0,0,kClientHeight,kClientWidth };
-
-	// クライアント領域を元に実際のサイズにwrcを更新してもらう
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-	// ウィンドウの生成
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,
-		L"CG2",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr,
-		nullptr,
-		wc.hInstance,
-		nullptr
-	);
-
-	ShowWindow(hwnd, SW_SHOW);
-
+	// ウィンドウ生成
+	WinApp* winApp = WinApp::GetInstance();
+	winApp->CreateGameWindow(1280, 720);
 	MSG msg{};
 
 	// DXGIファクトリーの生成
