8e5443fb7830ed2aaf24769346d7b4e1d0d4fabb
MatsumotoU <k024g0066@g.neec.ac.jp>
2025-02-10 10:33:25 +0900

    CG1_3

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 
+#include <dxgidebug.h>
+#pragma comment(lib,"dxguid.lib")
+
 int windowWidth = 1280;
 int windowHeight = 720;
 
@@ -261,6 +264,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			hr = commandList->Reset(commandAllocator, nullptr);
 			assert(SUCCEEDED(hr));
 		}
+	}
+
+	// * 解放処理 * //
+	CloseHandle(fenceEvent);
+	fence->Release();
+	rtvDescriptorHeap->Release();
+	swapChainResource[0]->Release();
+	swapChainResource[1]->Release();
+	swapChain->Release();
+	commandList->Release();
+	commandAllocator->Release();
+	commandQueue->Release();
+	device->Release();
+	useAdapter->Release();
+	dxgiFactory->Release();
+#ifdef _DEBUG
+	debugContoroller->Release();
+#endif // _DEBUG
+	CloseWindow(winApp->GetHWND());
+
+	// * 終了時のエラー処理 * //
+	IDXGIDebug1* debug;
+	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+		debug->Release();
 	}
 
 	return 0;
