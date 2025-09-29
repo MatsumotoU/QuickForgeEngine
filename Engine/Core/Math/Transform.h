9d3a83fe50aa98c39ad5897a671e991eb822e397
MatsumotoU <k024g0066@g.neec.ac.jp>
2025-09-30 02:54:14 +0900

    ギズモ追加

--- Engine/Core/Math/Transform.h
+++ Engine/Core/Math/Transform.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <nlohmann/json.hpp>
 #include "Vector/Vector3.h"
+#include "Matrix/Matrix4x4.h"
 #include "Core/Entity/Component/ComponentData.h"
 
 class Transform final : public ComponentData {
@@ -9,29 +10,8 @@ public:
     Vector3 rotate{ 0.0f, 0.0f, 0.0f };
     Vector3 translate{ 0.0f, 0.0f, 0.0f };
 
-    nlohmann::json Serialize() const override {
-        nlohmann::json json;
-        json["scale"] = { scale.x, scale.y, scale.z };
-        json["rotate"] = { rotate.x, rotate.y, rotate.z };
-        json["translate"] = { translate.x, translate.y, translate.z };
-        return json;
-    }
-	void Deserialize(const nlohmann::json& json) override {
-		if (json.contains("scale") && json["scale"].is_array() && json["scale"].size() == 3) {
-			scale.x = json["scale"][0].get<float>();
-			scale.y = json["scale"][1].get<float>();
-			scale.z = json["scale"][2].get<float>();
-		}
-		if (json.contains("rotate") && json["rotate"].is_array() && json["rotate"].size() == 3) {
-			rotate.x = json["rotate"][0].get<float>();
-			rotate.y = json["rotate"][1].get<float>();
-			rotate.z = json["rotate"][2].get<float>();
-		}
-		if (json.contains("translate") && json["translate"].is_array() && json["translate"].size() == 3) {
-			translate.x = json["translate"][0].get<float>();
-			translate.y = json["translate"][1].get<float>();
-			translate.z = json["translate"][2].get<float>();
-		}
-	}
+	void FromMatrix(const Matrix4x4& mat);
+	nlohmann::json Serialize() const override;
+    void Deserialize(const nlohmann::json& json) override;
     std::string GetTypeName() const override { return "Transform"; }
 };
