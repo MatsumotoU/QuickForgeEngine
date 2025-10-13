68d31d7f31aef80aac0c714ca6ee439d730f5bf1
MatsumotoU <k024g0066@g.neec.ac.jp>
2025-10-13 12:50:29 +0900

    gitignoreを先にコミット

--- GenerateBuildInfo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Gitî•ñ‚ðŽæ“¾‚µ‚ÄBuildInfo.h‚ð¶¬
-for /f %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
-for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set GIT_BRANCH=%%i
-
-echo #pragma once > BuildInfo.h
-echo #define BUILD_COMMIT "%GIT_COMMIT%" >> BuildInfo.h
-echo #define BUILD_BRANCH "%GIT_BRANCH%" >> BuildInfo.h
--- main.cpp
+++
@@ -1,5 +0,0 @@
-#include "QuickForgeEngine.h"
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int) {
-	QuickForgeEngine::RunOnWindows(hInstance, lpCmdLine);
-	return 0;
-}
