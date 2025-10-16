8f3cdbc7cc2983aa04849b3813d0eec1ba3c7634
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-10-16 13:49:31 +0900

    敵のシステムを修正

--- project/Resources/Scripts/FloatyEnemyMove.lua
+++ project/Resources/Scripts/FloatyEnemyMove.lua
@@ -1,12 +1,36 @@
 
+-- 速度
 speed = 0.0
+-- スロー速度
+local slowSpeed = 0.0
+-- 適応する速度
+local nowSpeed = 0.0
+
+-- ヒットフラグ
+local isHit = false
 
 function Init()
 DebugLog("Init")
+-- スロー速度を求める
+slowSpeed = speed * 0.5
 end
 
 function Update()
+    if isHit then
+        nowSpeed = slowSpeed
+    else
+        nowSpeed = speed
+    end
 
 -- 移動
-transform.translate.x = transform.translate.x - speed * (1.0 / 60.0)
+transform.translate.x = transform.translate.x - nowSpeed * (1.0 / 60.0)
+
+isHit = false
+end
+
+function OnCollisionEnter(id,obj)
+
+    if obj.tag == "SlowArea" then
+        isHit = true
+    end
 end
