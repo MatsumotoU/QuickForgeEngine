295bb6198eb6862edf606e281fc2d6aa6f936f02
MatsumotoU <k024g0066@g.neec.ac.jp>
2025-10-21 17:14:07 +0900

    モデルをロードするとテクスチャがバグるのを修正

--- project/Engine/Platform/Windows/Assets/3DModel/Loader/AssimpModelLoader.cpp
+++ project/Engine/Platform/Windows/Assets/3DModel/Loader/AssimpModelLoader.cpp
@@ -1,6 +1,11 @@
 #include "AssimpModelLoader.h"
 #include <cassert>
 
+#ifdef _DEBUG
+#include <format>
+#include "AppUtility/DebugTool/DebugLog/MyDebugLog.h"
+#endif // _DEBUG
+
 void AssimpModelLoader::LoadModelData(const std::string& modelResourceDirectory, const std::string& imageResourceDirectory, const std::string& filename, ModelData& modelData) {
 	Assimp::Importer importer;
 	std::string filepath = modelResourceDirectory + filename;
@@ -13,9 +18,20 @@ void AssimpModelLoader::LoadModelData(const std::string& modelResourceDirectory,
 		return;
 	}
 
+#ifdef _DEBUG
+	DebugLog(std::format("Model Load Success: {}", filepath));
+#endif // _DEBUG
+
 	for (unsigned int meshIdx = 0; meshIdx < scene->mNumMeshes; ++meshIdx) {
 		const aiMesh* mesh = scene->mMeshes[meshIdx];
 		MeshData meshData;
+
+#ifdef _DEBUG
+		DebugLog(std::format("Loading Mesh {} / {}", meshIdx + 1, scene->mNumMeshes));
+		DebugLog(std::format("UVChannel: {}", mesh->GetNumUVChannels()));
+		DebugLog(std::format("ColorChannel: {}", mesh->GetNumColorChannels()));
+		DebugLog(std::format("NumUVComponents for channel 0: {}", mesh->mNumUVComponents[0]));
+#endif // _DEBUG
 
 		// 頂点データ
 		std::vector<VertexData> tempVertices;
@@ -25,8 +41,9 @@ void AssimpModelLoader::LoadModelData(const std::string& modelResourceDirectory,
 			vtx.position.y = mesh->mVertices[i].y;
 			vtx.position.z = mesh->mVertices[i].z;
 			vtx.position.w = 1.0f;
+
 			if (mesh->HasTextureCoords(0)) {
-				vtx.texcoord.x = 1.0f - mesh->mTextureCoords[0][i].x;
+				vtx.texcoord.x = mesh->mTextureCoords[0][i].x;
 				vtx.texcoord.y = mesh->mTextureCoords[0][i].y;
 			} else {
 				vtx.texcoord.x = 0.0f;
@@ -55,7 +72,6 @@ void AssimpModelLoader::LoadModelData(const std::string& modelResourceDirectory,
 				meshData.vertices.push_back(tempVertices[face.mIndices[0]]);
 				meshData.vertices.push_back(tempVertices[face.mIndices[2]]);
 				meshData.vertices.push_back(tempVertices[face.mIndices[1]]);
-
 			}
 		}
 
@@ -66,7 +82,6 @@ void AssimpModelLoader::LoadModelData(const std::string& modelResourceDirectory,
 			if (material->GetTexture(aiTextureType_DIFFUSE, 0, &texPath) == AI_SUCCESS) {
 				meshData.material.textureFilePath = imageResourceDirectory + std::string(texPath.C_Str());
 			}
-			// ここで他のマテリアル情報（色など）も必要なら取得可能
 		}
 
 		modelData.meshes.push_back(std::move(meshData));
