a9fc29bc288953d936f992df0768245524b2a60d
MatsumotoU <k024g0066@g.neec.ac.jp>
2025-10-23 00:01:43 +0900

    当たり判定のLayyerを修正

--- project/Engine/Resources/Shaders/PS/Object3d.PS.hlsl
+++ project/Engine/Resources/Shaders/PS/Object3d.PS.hlsl
@@ -17,7 +17,6 @@ SamplerState gSampler : register(s0);
 struct PixelShaderOutput{
     float32_t4 color : SV_TARGET0;
 };
-
 PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
