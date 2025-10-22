c5d1998cd36e6732737948c234e480b3f598d008
tenma <yakiimo1344@yahoo.co.jp>
2025-10-23 02:42:45 +0900

    lua mastered

--- project/Resources/Scripts/ColoerTest.lua
+++ project/Resources/Scripts/ColoerTest.lua
@@ -1,9 +1,11 @@
 local mat = Material.new()
 
 function Init()
+
     mat = GetMaterial(GetThisEntityId())
     mat.color.x = 0.5
     DebugLog("Init")
+
 end
 
 function Update()
--- project/Resources/Scripts/EnemyHp.lua
+++ project/Resources/Scripts/EnemyHp.lua
@@ -31,41 +31,41 @@ end
 
 function Update()
 
-    if not isAlive then 
-        destroy()
-    end
+    if isAlive then 
 
-    time = time + 1.0
+        time = time + 1.0
 
-    if damageInterval > 0.0 then
-        damageInterval = damageInterval - 0.016
-        transform.scale.y = math.sin(time) * math.sin(time)
-    else
-        transform.scale.y = 1.0
-    end
+        if damageInterval > 0.0 then
+            damageInterval = damageInterval - 0.016
+            transform.scale.y = math.sin(time) * math.sin(time)
+        else
+            transform.scale.y = 1.0
+        end
 
-    -- カメラの追跡する位置を設定
-    targetTransform = GetTransform(targetId)
-    local targetX = targetTransform.translate.x
+        -- カメラの追跡する位置を設定
+        targetTransform = GetTransform(targetId)
+        local targetX = targetTransform.translate.x
 
-    --DebugLog("Id"..targetId)
-    --DebugLog("cameraLeftBoderX"..targetX)
-    --DebugLog("enemyPosX"..transform.translate.x)
+        --DebugLog("Id"..targetId)
+        --DebugLog("cameraLeftBoderX"..targetX)
+        --DebugLog("enemyPosX"..transform.translate.x)
 
-    -- 画面左端を出たら生存フラグをfalse
-    if transform.translate.x <= targetX then
-        DebugLog("EnemyIsAliveFalse")
-        isAlive = false
+        -- 画面左端を出たら生存フラグをfalse
+        if transform.translate.x <= targetX then
+            DebugLog("EnemyIsAliveFalse")
+            isAlive = false
 
-        -- 通常の敵だった場合、通常の幽霊を生成
-        if enemyType == 0 then
-             local temp = Transform.new()
-            temp.translate.x = transform.translate.x + 1.0
-            temp.translate.y = transform.translate.y + 2.0
-            temp.translate.z = transform.translate.z
-            CreateEntity(normalEnemyName,temp)
+            -- 通常の敵だった場合、通常の幽霊を生成
+            if enemyType == 0 then
+                local temp = Transform.new()
+                temp.translate.x = transform.translate.x + 1.0
+                temp.translate.y = transform.translate.y + 2.0
+                temp.translate.z = transform.translate.z
+                CreateEntity(normalEnemyName,temp)
+            end
         end
     end
+
 end
 
 function OnCollisionEnter(id,obj)
