319cfe0f734d109cd2bee3774a604631028feed3
yutam <yuta.mr1011@gmail.com>
2025-12-22 22:02:31 +0900

    タイムライン機能をインクルード

--- project/engine/include/utility/DebugTool/ImGui/ImGuiInclude.h
+++ project/engine/include/utility/DebugTool/ImGui/ImGuiInclude.h
@@ -3,7 +3,8 @@
 #include "Externals/imgui/imgui_impl_dx12.h"
 #include "Externals/imgui/imgui_impl_win32.h"
 
-#include "Externals/imgui/imgui_toggle-main/imgui_toggle.h"// 荳驛ｨ隴ｦ蜻顔┌隕・
+#include "Externals/imgui/imgui_toggle-main/imgui_toggle.h"
 #include "Externals/imgui/implot-master/implot.h"
-#include "Externals/imgui/imgui-knobs-main/imgui-knobs.h"// 縺九↑繧翫・謨ｰ縺ｮ隴ｦ蜻顔┌隕・imgui-knobs.cpp隕∝盾辣ｧ)
+#include "Externals/imgui/imgui-knobs-main/imgui-knobs.h"
 #include "Externals/imgui/ImGuizmo-1.83/ImGuizmo.h"
+#include "Externals/imgui/ImGuizmo-1.83/ImSequencer.h"
--- project/engine/include/utility/FileSystems/FileUtility.h
+++ project/engine/include/utility/FileSystems/FileUtility.h
@@ -9,11 +9,16 @@
 #include <nlohmann/json.hpp>
 
 namespace QFE::FILE {
+	/// ある階層のディレクトリ内にある特定拡張子のファイル名一覧を取得します
 	extern std::vector<std::string> GetFilesInDirectory(const std::string& directoryPath, const std::string& extension = "");
+	/// 指定した実行ファイルで特定のファイルを開きます
 	extern bool OpenFileOnExe(const std::string& exePath, const std::string& filePath);
+	/// 指定したファイルをJSON形式で読み込みます
 	extern bool LoadFileToJson(const std::string& filePath, nlohmann::json& json);
+	/// ファイル名が特定の拡張子を持っているか確認します
 	bool HasExtension(const std::string& fileName, const std::string& extension);
-
+	/// CSVファイルを2次元のuint32_tベクトルに読み込みます
 	extern bool LoadCSVToVector(const std::string& filePath, std::vector<std::vector<uint32_t>>& map);
+	/// JSON形式のデータを指定したファイルに保存します
 	extern bool SaveJSONToFile(const std::string& filePath, const nlohmann::json& json);
 }
