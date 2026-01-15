fff3e5b1343300d91aac72f3f20b098db4803bb8
yutam <yuta.mr1011@gmail.com>
2026-01-15 23:10:48 +0900

    使っていないよくわからない乱数クラスを削除

--- project/Engine/BuildInfo.h
+++ project/Engine/BuildInfo.h
@@ -1,3 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "5a8ae5f6" 
+#define BUILD_COMMIT "70b5bfa1" 
 #define BUILD_BRANCH "develop" 
+#define BUILD_DATE "2026/01/15" 
+#define BUILD_TIME "23:10:11.18" 
