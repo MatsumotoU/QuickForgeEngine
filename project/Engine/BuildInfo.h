9287bacb2a519dc7094e975e25690347820b88e2
yutam <yuta.mr1011@gmail.com>
2026-01-16 15:46:30 +0900

    デバッグログクラスがコンストラクタとデストラクタでファイル操作していたのを修正。ファイル操作をしている部分に例外処理を入れた

--- project/Engine/BuildInfo.h
+++ project/Engine/BuildInfo.h
@@ -1,5 +1,4 @@
 #pragma once 
-#define BUILD_COMMIT "6544fcbd" 
+#define BUILD_COMMIT "673bc74a" 
 #define BUILD_BRANCH "develop" 
 #define BUILD_DATE "2026/01/16" 
-#define BUILD_TIME "15:22:13.58" 
