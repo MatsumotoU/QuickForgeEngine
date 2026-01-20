68372439acc6e309f1650cf578a5137d30bc8df9
MatsumotoU <k024g0066@g.neec.ac.jp>
2026-01-20 12:30:24 +0900

    エディタ側のbuildInfoを削除

--- project/Engine/BuildInfo.h
+++ project/Engine/BuildInfo.h
@@ -1,5 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "5c5f7e9d" 
+#define BUILD_COMMIT "9d5b66cd" 
 #define BUILD_BRANCH "develop" 
 #define BUILD_DATE "2026/01/20" 
-#define BUILD_TIME "11:58:58.04" 
+#define BUILD_TIME "12:29:18.71" 
