20b0ac3e6ef61e25cc0ffbe44bf7a65707e620a9
MatsumotoU <k024g0066@g.neec.ac.jp>
2026-01-26 12:18:25 +0900

    エンティティを削除したときにC#スクリプトも消してくれるように変更

--- project/Engine/BuildInfo.h
+++ project/Engine/BuildInfo.h
@@ -1,5 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "a7b7b982" 
+#define BUILD_COMMIT "3559684f" 
 #define BUILD_BRANCH "develop" 
 #define BUILD_DATE "2026/01/26" 
-#define BUILD_TIME "12:04:50.84" 
+#define BUILD_TIME "12:15:31.78" 
