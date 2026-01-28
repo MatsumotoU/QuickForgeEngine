1edfdfd0bc73e146e46d03f9def61662e675742d
MatsumotoU <k024g0066@g.neec.ac.jp>
2026-01-28 10:47:05 +0900

    premake.exeをGitHubから削除、premake.batを起動したときになかったらDLする方式に変更

--- project/premake5.bat
+++ project/premake5.bat
@@ -8,6 +8,17 @@ set PREMAKE_EXE=premake5.exe
 
 set TARGET_IDE=vs2022
 
+if not exist "%PREMAKE_EXE%" (
+    echo [0/2] %PREMAKE_EXE% not found. Downloading...
+    powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $url = 'https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip'; $zip = 'premake.zip'; Invoke-WebRequest -Uri $url -OutFile $zip; $shell = New-Object -ComObject Shell.Application; $zipFile = $shell.NameSpace((Get-Item $zip).FullName); $dest = $shell.NameSpace((Get-Item .).FullName); $dest.CopyHere($zipFile.Items(), 16); Remove-Item $zip"
+    if not exist "%PREMAKE_EXE%" (
+        echo Error: Failed to download %PREMAKE_EXE%
+        pause
+        exit /b 1
+    )
+    echo Download Complete.
+)
+
 
 
 echo ====================================
