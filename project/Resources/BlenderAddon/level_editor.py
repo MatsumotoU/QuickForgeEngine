b80555855444ef9c1490915c3b50bef834cf70f1
MatsumotoU <k024g0066@g.neec.ac.jp>
2026-04-21 12:17:31 +0900

    トップバーにカスタムメニュー「MyMenu」を追加
    Blenderアドオン level_editor.py に TOPBAR_MT_my_menu クラスを新規追加し、トップバーのエディタメニューに「MyMenu」サブメニューを表示する機能を実装。register/unregister 関数でクラスの登録・解除とメニューの追加・削除を管理し、アドオンの有効化/無効化に連動してメニューが表示・非表示となるようにした。

--- project/Resources/BlenderAddon/level_editor.py
+++ project/Resources/BlenderAddon/level_editor.py
@@ -13,8 +13,32 @@ bl_info = {
     "category": "Object"
 }
 
-def register():
-    print("enable LevelEditor")
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
 
+    def draw(self,context):
+        self.layout.operator("wm.url_open_preset",
+            text = "Manual",icon = 'HELP')
+        
+    def submenu(self,context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+classes = {
+    TOPBAR_MT_my_menu,
+}
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    print("enable LevelEditor")
+    
 def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("unenable LevelEditor")
