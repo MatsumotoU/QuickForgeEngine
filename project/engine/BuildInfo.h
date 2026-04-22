84c9060c448f12060f4715733ad94a2ead21a5d5
MatsumotoU <k024g0066@g.neec.ac.jp>
2026-04-22 15:11:02 +0900

    Luaスクリプトディレクトリ取得機能とリスト更新追加
    InspectorViewの「AddScript」メニューに「Refresh List」を追加し、Luaスクリプト一覧の再読み込みを可能にしました。EditorEngineBridgeにLuaスクリプトディレクトリ取得用関数を追加・登録しました。その他、ビルド情報の更新と軽微な整形を行いました。

--- project/engine/BuildInfo.h
+++ project/engine/BuildInfo.h
@@ -1,5 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "7408696c" 
+#define BUILD_COMMIT "ae518bef" 
 #define BUILD_BRANCH "develop" 
-#define BUILD_DATE "2026/04/21" 
-#define BUILD_TIME "15:59:01.19" 
+#define BUILD_DATE "2026/04/22" 
+#define BUILD_TIME "14:45:41.01" 
