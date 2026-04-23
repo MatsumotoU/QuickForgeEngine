db6d14075fee0527836c780c3db2875b80cc87ea
yutam <yuta.mr1011@gmail.com>
2026-04-23 22:39:45 +0900

    AssimpモデルローダーのMeshData初期化方法を修正
    - MeshData構造体にサイズ指定コンストラクタを追加
    - AssimpModelLoaderでMeshData生成時に頂点数で初期化するよう変更
    - KeyConfig.json, SceneConfig.jsonの内容を削除
    - NuGet関連ファイルを削除
    - EngineConfig.jsonのlastProjectNameを"XEVIOUS3D"に変更
    - ビルド情報（コミットID・日付・時刻）を更新

--- project/engine/BuildInfo.h
+++ project/engine/BuildInfo.h
@@ -1,5 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "fb50cfb8" 
+#define BUILD_COMMIT "0bae83e1" 
 #define BUILD_BRANCH "develop" 
-#define BUILD_DATE "2026/04/22" 
-#define BUILD_TIME "21:11:51.72" 
+#define BUILD_DATE "2026/04/23" 
+#define BUILD_TIME "22:29:19.28" 
