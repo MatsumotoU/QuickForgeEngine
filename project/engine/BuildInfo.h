f7f14c70cac587f26238a199dc158ce91992cf4d
MatsumotoU <k024g0066@g.neec.ac.jp>
2026-05-08 13:33:14 +0900

    C#エンティティ取得/削除API追加とSafeVector強化
    C#からエンティティ名で取得・削除できるAPIを追加し、MonoRuntimeManagerで内部呼び出しを登録しました。SafeVectorで例外発生時に__debugbreak()を呼ぶようにし、デバッグ性を向上。エンティティ名取得時の戻り値をUINT32_MAXに変更し、見つからない場合の安全性を強化しました。その他、ビルド情報や細かなコード整理も実施。

--- project/engine/BuildInfo.h
+++ project/engine/BuildInfo.h
@@ -1,5 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "546d7b52" 
+#define BUILD_COMMIT "62a62190" 
 #define BUILD_BRANCH "develop" 
 #define BUILD_DATE "2026/05/07" 
-#define BUILD_TIME "08:28:20.93" 
+#define BUILD_TIME "15:08:48.25" 
