c13c4692a580e055db969b8863eb3e60d7ef2a69
yutam <yuta.mr1011@gmail.com>
2026-05-09 16:07:34 +0900

    コリジョン判定関数の分離とリファクタリング
    - ColliderManager内の衝突判定関数をCollisionDetection.*へ分離
    - QFE::COLLIDER名前空間のグローバル関数として実装
    - 呼び出し箇所を新関数に置換
    - コメントを日本語で明確化・詳細化
    - ビルド情報の自動更新
    - 機能的な変更はなく、可読性・保守性の向上が目的

--- project/engine/BuildInfo.h
+++ project/engine/BuildInfo.h
@@ -1,5 +1,5 @@
 #pragma once 
-#define BUILD_COMMIT "b7e45fbd" 
+#define BUILD_COMMIT "ee5c0370" 
 #define BUILD_BRANCH "develop" 
-#define BUILD_DATE "2026/05/08" 
-#define BUILD_TIME "07:37:49.82" 
+#define BUILD_DATE "2026/05/09" 
+#define BUILD_TIME "07:13:04.73" 
--- project/engine/include/collider/ColliderManager.h
+++ project/engine/include/collider/ColliderManager.h
@@ -18,10 +18,6 @@ namespace QFE {
 		ColliderTagMask colliderTagMask_;
 
 	private:
-		bool isCollision(const Sphere& sphere1, const Sphere& sphere2);
-		bool isCollision(const AABB& aabb1, const AABB& aabb2);
-		bool isCollision(const Sphere& sphere, const AABB& aabb);
-
 		void SphereToSphereUpdate();
 		void AABBToAABBUpdate();
 		void SphereToAABBUpdate();
