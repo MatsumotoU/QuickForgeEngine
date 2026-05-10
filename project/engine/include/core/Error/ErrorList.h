b9d47084b052e7cdb0d18e33ad54ddf11e22b643
yutam <yuta.mr1011@gmail.com>
2026-05-11 07:31:09 +0900

    エラーの止め方を実装しようとしている - Userエラーは止めない安全装置 - Systemエラーは絶対落とす これを実装する

--- project/engine/include/core/Error/ErrorList.h
+++ project/engine/include/core/Error/ErrorList.h
@@ -1,12 +1,37 @@
 #pragma once
+#include <string>
 
 namespace QFE {
-
-	enum class ErrorList {
-		OK,
-		FAILED,
-		ERR_FILE_NOT_FOUND,
-		ERR_FILE_BAD_PATH,
+	// エラーの分類は以下の通り
+	// 
+	// * exeを落とさないエラー * //
+	// UserError: ユーザーの操作や入力に起因するエラー
+	// -- PlayerError: プレイヤーの操作に関連するエラー
+	// -- DeveloperError: 開発者のコードや設定に起因するエラー
+	// 
+	// * exeを落とすエラー * //
+	// SystemError: システムや環境に起因するエラー
+	// -- Abort: プログラムの異常終了を引き起こすエラー
+	
+	// エラーの種類を定義する列挙型
+	enum class ErrorType {
+		UserError,
+		SystemError
 	};
+	// ユーザーエラーの種類を定義する列挙型
+	enum class UserError {
+		PlayerError,
+		DeveloperError
+	};
+	// システムエラーの種類を定義する列挙型
+	enum class SystemError {
+		Abort
+	};
+
+	/// @brief ユーザーエラーを報告する関数
+	void ReportUserError(const std::string& message, UserError error);
+
+	/// @brief システムエラーを報告する関数
+	void ReportSystemError(const std::string& message, SystemError error);
 
 }
---
+++ project/engine/src/core/Error/ErrorList.cpp
@@ -0,0 +1,9 @@
+#include "engine/include/core/Error/ErrorList.h"
+
+void QFE::ReportUserError(const std::string& message, UserError error)
+{
+}
+
+void QFE::ReportSystemError(const std::string& message, SystemError error)
+{
+}
